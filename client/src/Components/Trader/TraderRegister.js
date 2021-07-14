d7c28a9d962cd356e0c997ec57e4ca8d549faace
celine <celine.ly.99@outlook.fr>
2021-07-14 13:49:12 +0200

    calendar

--- client/src/Components/Trader/TraderRegister.js
+++ client/src/Components/Trader/TraderRegister.js
@@ -1,20 +1,45 @@
 import { Select, TextField, MenuItem, Button } from '@material-ui/core';
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-
+import axios from "axios";
+import { useState } from "react";
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+import 'date-fns';
 
 
 export default function TraderRegister(){
-    const useStyles = makeStyles((theme) => ({
-    }))
     const [value, setValue] = React.useState('')
     const handleChange = (event) => {
         setValue(event.target.value)
     }
+
+    const initialvalues = {
+        companyName: "",
+        kbis: "",
+        devise: "",
+        contactEmail: "",
+        username: "",
+    }
+    const [listOfTraders, setListOfTraders] = useState([]);
+    const onSubmit = (data) => {
+        axios.post("http://localhost:3000/traders", data).then((response) => {
+            // setListOfTraders(response.data);
+            console.log("DONE ENVOIE");
+        })
+    }
+    const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
+
+    const handleDateChange = (date) => {
+      setSelectedDate(date);
+    };
     return (
        <>
        <div className="registerTraderContainer">
-        <form className="registerTrader" noValidate autoComplete="off">
+        <form className="registerTrader" autoComplete="off" initialvalues={initialvalues} onSubmit={onSubmit}>
             <TextField id="standard-required" name="companyName" label="Company Name" />
             <TextField id="standard-required" name="kbis" label="KBIS" />
             <Select name="devise" id="standard-select-currency" value={value} onChange={handleChange}>
@@ -30,7 +55,41 @@ export default function TraderRegister(){
             type="password"
             autoComplete="current-password"
             />
-            <Button color="primary">S'inscrire</Button>
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <Grid container justifyContent="space-around">
+        <KeyboardDatePicker
+          disableToolbar
+          variant="inline"
+          format="MM/dd/yyyy"
+          margin="normal"
+          id="date-picker-inline"
+          label="CreatedAt"
+          name="createdAt"
+          value={selectedDate}
+          onChange={handleDateChange}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        /> </Grid>
+        </MuiPickersUtilsProvider>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <Grid container justifyContent="space-around">
+        <KeyboardDatePicker
+          disableToolbar
+          variant="inline"
+          format="MM/dd/yyyy"
+          margin="normal"
+          id="date-picker-inline"
+          label="updatedAt"
+          name="updatedAt"
+          value={selectedDate}
+          onChange={handleDateChange}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        /> </Grid>
+        </MuiPickersUtilsProvider>
+            <Button type="submit" color="primary"> S'inscrire</Button>
         </form>
        </div>
        </>
