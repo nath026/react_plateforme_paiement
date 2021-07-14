a625438b9444e22b084e88f67cfd90885d07a64d
celine <celine.ly.99@outlook.fr>
2021-07-14 17:04:26 +0200

    login trader

--- client/src/Components/Trader/TraderLogin.js
+++ client/src/Components/Trader/TraderLogin.js
@@ -1,7 +1,29 @@
-import Login from "../lib/Login";
+import React, {useState} from "react";
+import axios from "axios";
 
 export default function TraderLogin(){
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const login = () => {
+        const data = { username: username, password: password};
+        axios.post("http://localhost:3000/traders/login", data).then((response) => {
+            console.log(response.data);
+        })
+    }
     return (
-        <Login/>
+        <>
+        <input type="text"
+        onChange={(event) => {
+            setUsername(event.target.value);
+        }}
+        />
+        <input type="password"
+        onChange={(event) => {
+            setPassword(event.target.value)}}
+        />
+
+        <button onClick={login}> Login </button>
+        </>
     );
 }
