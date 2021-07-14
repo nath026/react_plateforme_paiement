5f9385ec6e7222cc04216ec6c67a8fb06f18dc3e
celine <celine.ly.99@outlook.fr>
2021-07-14 14:50:50 +0200

    remove material ui and fix submit

--- client/src/Components/Trader/TraderRegister.js
+++ client/src/Components/Trader/TraderRegister.js
@@ -1,15 +1,8 @@
-import { Select, TextField, MenuItem, Button } from '@material-ui/core';
+// import { Select, TextField, MenuItem, Button } from '@material-ui/core';
 import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import axios from "axios";
 import { useState } from "react";
-import Grid from '@material-ui/core/Grid';
-import DateFnsUtils from '@date-io/date-fns';
-import {
-  MuiPickersUtilsProvider,
-  KeyboardDatePicker,
-} from '@material-ui/pickers';
-import 'date-fns';
-
 
 export default function TraderRegister(){
     const [value, setValue] = React.useState('')
@@ -17,80 +10,93 @@ export default function TraderRegister(){
         setValue(event.target.value)
     }
 
-    const initialvalues = {
+    const initialValues = {
         companyName: "",
         kbis: "",
         devise: "",
         contactEmail: "",
         username: "",
+        password: "",
+        updatedAt:"",
+        createdAt:""
     }
     const [listOfTraders, setListOfTraders] = useState([]);
     const onSubmit = (data) => {
+        console.log(data);
         axios.post("http://localhost:3000/traders", data).then((response) => {
-            // setListOfTraders(response.data);
-            console.log("DONE ENVOIE");
+            setListOfTraders(response.data);
+            // console.log("DONE ENVOIE");
         })
-    }
-    const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
 
-    const handleDateChange = (date) => {
-      setSelectedDate(date);
-    };
+    }
     return (
        <>
        <div className="registerTraderContainer">
-        <form className="registerTrader" autoComplete="off" initialvalues={initialvalues} onSubmit={onSubmit}>
-            <TextField id="standard-required" name="companyName" label="Company Name" />
-            <TextField id="standard-required" name="kbis" label="KBIS" />
-            <Select name="devise" id="standard-select-currency" value={value} onChange={handleChange}>
-                <MenuItem value={'EUR'}> EUR </MenuItem>
-                <MenuItem value={'USD'}> USD </MenuItem>
-                <MenuItem value={'YEN'}> YEN </MenuItem>
-            </Select>
-            <TextField id="standard-required" name="contactEmail" label="Contact Email" />
-            <TextField id="standard-required" name="username" label="Username" />
-            <TextField
-            id="password"
-            label="Password"
-            type="password"
-            autoComplete="current-password"
-            />
-            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-      <Grid container justifyContent="space-around">
-        <KeyboardDatePicker
-          disableToolbar
-          variant="inline"
-          format="MM/dd/yyyy"
-          margin="normal"
-          id="date-picker-inline"
-          label="CreatedAt"
-          name="createdAt"
-          value={selectedDate}
-          onChange={handleDateChange}
-          KeyboardButtonProps={{
-            'aria-label': 'change date',
-          }}
-        /> </Grid>
-        </MuiPickersUtilsProvider>
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-      <Grid container justifyContent="space-around">
-        <KeyboardDatePicker
-          disableToolbar
-          variant="inline"
-          format="MM/dd/yyyy"
-          margin="normal"
-          id="date-picker-inline"
-          label="updatedAt"
-          name="updatedAt"
-          value={selectedDate}
-          onChange={handleDateChange}
-          KeyboardButtonProps={{
-            'aria-label': 'change date',
-          }}
-        /> </Grid>
-        </MuiPickersUtilsProvider>
-            <Button type="submit" color="primary"> S'inscrire</Button>
-        </form>
+       <Formik initialValues={initialValues} onSubmit={onSubmit} >
+            <Form>
+                <label>Nom de la company : </label>
+                <Field 
+                    autocomplete="off"
+                    id="inputRegisterTrader"
+                    name="companyName"
+                    placeholter="Company Name"
+                />
+                 <label>KBIS </label>
+                <Field 
+                    autocomplete="off"
+                    id="inputRegisterTrader"
+                    name="kbis"
+                    placeholter="kbis"
+                />
+                 <label>Devise </label>
+                <Field 
+                    autocomplete="off"
+                    id="inputRegisterTrader"
+                    name="devise"
+                    as="select"
+                ><option value="EUR">EUR</option>
+                <option value="YEN">YEN</option>
+                <option value="USD">USD</option>
+             </Field>
+                 <label>Contact Email </label>
+                <Field 
+                    autocomplete="off"
+                    id="inputRegisterTrader"
+                    name="contactEmail"
+                    placeholter="Contact email"
+                />
+                 <label>Username </label>
+                <Field 
+                    autocomplete="off"
+                    id="inputRegisterTrader"
+                    name="username"
+                    placeholter="username"
+                />
+                 <label>password </label>
+                <Field 
+                    autocomplete="off"
+                    id="inputRegisterTrader"
+                    name="password"
+                    placeholter="password"
+                    type="password"
+                />
+                  <label>CreatedAt </label>
+                <Field 
+                    autocomplete="off"
+                    id="inputRegisterTrader"
+                    name="createdAt"
+                    placeholter="createdAt"
+                />
+                  <label>Updated At </label>
+                <Field 
+                    autocomplete="off"
+                    id="inputRegisterTrader"
+                    name="updatedAt"
+                    placeholter="updatedAt"
+                />
+                <button type="submit"> Envoyer </button>
+            </Form>
+       </Formik>
        </div>
        </>
     );
