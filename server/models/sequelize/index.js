6610ecee3a8ae149bbfef76cc9d85d5a9ac566c1
nath026 <71986720+nath026@users.noreply.github.com>
2021-07-17 19:49:39 +0200

    correcting conflic index

--- server/models/sequelize/index.js
+++ server/models/sequelize/index.js
@@ -4,38 +4,10 @@ const Transaction = require('./Transaction');
 const User = require('./User');
 const Article = require('./Article');
 const Trader = require('./Trader');
-<<<<<<< HEAD
+const Order = require('./Order');
 const TraderArticle = require('../model/TraderArticle');
 const UserArticle = require('../model/UserArticle');
-=======
-const Order = require('./Order');
->>>>>>> 332baf055e0c00460a8e1f840b02242667b991f3
 const connection = require('../../lib/sequelize');
-
-// const denormalizeUser = (user) => {
-//   User.findByPk(user.id, {
-//     include: [{ model: Article, attributes: ['id', 'title', 'createdAt'] }],
-//   }).then((data) => {
-//     const denormalizedUser = data.toJSON();
-//     denormalizedUser._id = denormalizedUser.id;
-//     UserArticle.findOneAndReplace(
-//       { _id: denormalizedUser.id },
-//       denormalizedUser,
-//       { upsert: true, new: true },
-//     ).then((data) => console.log(`User ${data._id} saved to mongo`));
-//   });
-// };
-
-// const denormalizeTrader = (trader) => {
-//   Trader.findByPk(trader.id).then((data) => data.toJSON()).save();
-// };
-
-<<<<<<< HEAD
-const denormalizeTradertoArticle = (trader) => {
-  Trader.findByPk(trader.id, {
-    include: [{ model: Article, as: 'myArticles' }],
-  }).then((data) => new TraderArticle({ _id: data.id, ...data.toJSON() }));
-};
 
 const denormalizeTrader = (trader) => {
   Trader.findByPk(trader.id, {
@@ -51,22 +23,11 @@ const denormalizeTrader = (trader) => {
   });
 };
 
-// User.addHook('afterUpdate', denormalizeUser);
-// User.addHook('afterCreate', denormalizeUser);
-Trader.addHook('afterUpdate', denormalizeTrader);
-Trader.addHook('afterCreate', denormalizeTrader);
-Article.addHook('afterUpdate', (article) => denormalizeTradertoArticle(article.author));
-Article.addHook('afterCreate', (article) => denormalizeTradertoArticle(article.author));
-=======
 const denormalizeOrder = (order) => {
   Trader.findByPk(trader.id).then((data) => data.toJSON()).save();
 };
->>>>>>> 332baf055e0c00460a8e1f840b02242667b991f3
 
-async function init ()  
-{
-  User.addHook('afterUpdate', denormalizeUser);
-  User.addHook('afterCreate', denormalizeUser);
+async function init() {
   Trader.addHook('afterUpdate', denormalizeTrader);
   Trader.addHook('afterCreate', denormalizeTrader);
   Article.addHook('afterUpdate', (article) => denormalizeUser(article.author));
@@ -80,21 +41,17 @@ async function init ()
   // relations
   Article.belongsTo(Trader, { as: 'trader' });
   Transaction.hasMany(Article);
-  
+
   Order.hasOne(Transaction);
-  
-  Article.belongsTo(Transaction, { as: "multipleArticle" }); 
+
+  Article.belongsTo(Transaction, { as: 'multipleArticle' });
   Transaction.hasMany(Article);
 
-  await connection.sync({force: true});
-  console.log('Database sync')
-  
+  await connection.sync({ force: true });
+  console.log('Database sync');
 }
 
-init()
-
-
-
+init();
 
 module.exports = {
   User,
