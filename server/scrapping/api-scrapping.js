08a0b04fe39eb99632453a2f52f4525b9c6013a9
nath026 <nathalie.ton026@gmail.com>
2021-07-26 21:27:50 +0200

    fetch data from the api and save it to a json file

---
+++ server/scrapping/api-scrapping.js
@@ -0,0 +1,66 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+
+const euroArray = [];
+
+function formatDate(date) {
+  const d = new Date(date);
+  let month = `${d.getMonth() + 1}`;
+  let day = `${d.getDate()}`;
+  const year = d.getFullYear();
+  if (month.length < 2) month = `0${month}`;
+  if (day.length < 2) day = `0${day}`;
+  return [year, month, day].join('-');
+}
+// eslint-disable-next-line no-extend-native
+Date.prototype.addDays = function addDays(days) {
+  const date = new Date(this.valueOf());
+  date.setDate(date.getDate() + days);
+  return date;
+};
+async function getData(date) {
+  const reponse = await fetch(
+    `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/${date}/currencies/eur.min.json`,
+  );
+  if (reponse.status >= 300) throw new Error('Something went wrong');
+  // save the data in an array
+  const dataFetch = await reponse.json();
+  console.log(date);
+  console.log('yen : ', dataFetch.eur.jpy);
+  console.log('usd : ', dataFetch.eur.usd);
+
+  const euroDateData = {
+    date: new Date(date),
+    currency: {
+      YEN: dataFetch.eur.jpy,
+      USD: dataFetch.eur.usd,
+    },
+  };
+  euroArray.push(euroDateData);
+  const euroArrayObjet = JSON.stringify(euroArray);
+  // console.log('array', euroArray);
+  fs.writeFile('./euroConvertData.json', euroArrayObjet, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log('The file was saved!');
+  });
+  return console.log(`data save for ${date}`);
+}
+
+async function main() {
+  try {
+    const today = new Date();
+    let date = new Date('2020-11-22');
+    while (date <= today) {
+      const dateformat = formatDate(date);
+      // eslint-disable-next-line no-await-in-loop
+      await getData(dateformat);
+      const nextDate = date.addDays(1);
+      date = nextDate;
+    }
+  } catch (error) {
+    console.error(` ${error.message}`);
+  }
+}
+main().catch(console.error);
