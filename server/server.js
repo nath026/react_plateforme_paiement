44a03bcfb7e901dc9e5330a9a268648518417542
nath026 <nathalie.ton026@gmail.com>
2021-07-28 21:02:32 +0200

    clean up server file

--- server/server.js
+++ server/server.js
@@ -3,29 +3,10 @@
 const express = require('express');
 const cors = require('cors');
 const { authRole } = require('./middleware/verifyAuthorization');
-const router = require("express").Router();
-const fetch = require('node-fetch');
 
 const app = express();
 
-
-
-app.use(cors()) // Use this after the variable declaration
-
-router.post('/psp', (req, res) => {
-    res.sendStatus(202);
-    setTimeout( ()=>{
-        fetch(process.env.API_URL+'/webbook', { method: 'POST', body: "message='payment is accepted'" })
-            .then(res => res.json()) // expecting a json response
-            .then(json => console.log(json));
-    },10000);
-});
-
-
-app.listen(3000, () => console.log("psp is listening"));
-require('dotenv').config();
-
-const app = express();
+app.use(cors()); // Use this after the variable declaration
 
 const TraderRouter = require('./routes/TraderRouter');
 const ArticleRouter = require('./routes/ArticleRouter');
